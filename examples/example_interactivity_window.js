d092a3e41b6119e9aafadc9af9c97a299b791454
Vladimir Sumarov <summeroff@gmail.com>
2020-11-03 15:23:37 -0800

    fix module load

--- examples/example_interactivity_window.js
+++ examples/example_interactivity_window.js
@@ -1,6 +1,6 @@
 
 const { app, BrowserWindow } = require("electron")
-const streamlabs_overlays = require('../build/Debug/streamlabs_overlay.node')
+const streamlabs_overlays = require('../build/Debug/game-overlay.node')
 const fs = require("fs")
 
 let win;
