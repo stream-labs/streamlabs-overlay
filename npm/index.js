b4d32de8aa465ab4d3bacc57729efe11c2e5c83d
Vladimir Sumarov <summeroff@gmail.com>
2020-11-03 15:10:40 -0800

    fix module name

--- npm/index.js
+++ npm/index.js
@@ -1,9 +1,5 @@
 const path = require('path');
-//const bindingPath = binary.find(path.resolve(path.join(__dirname, './package.json')));
-const bindingPath = path.resolve(path.join(__dirname, './streamlabs_overlay.node'))
+const bindingPath = path.resolve(path.join(__dirname, './game_overlay.node'))
 const binding = require(bindingPath); 
-
-//console.log( path.resolve(path.join(__dirname, './streamlabs_overlay.node')) );
-//const binding = require('C:/work/repos/streamlabs-overlay/build-tmp-napi-v3/Release/streamlabs_overlay.node'); 
 
 module.exports = binding;
