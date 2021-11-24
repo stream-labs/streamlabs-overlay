4abda843a3b1fd9f023ba94df97f9ca2c1f95854
Vladimir Sumarov <summeroff@gmail.com>
2021-11-24 15:19:44 -0800

    debug linking

--- scripts/check_dependencies.cmd
+++ scripts/check_dependencies.cmd
@@ -1,4 +1,3 @@
-echo "test text echoed from script"
 dumpbin /DEPENDENTS %1 > %2/current_dependents_s1.txt
 powershell -command "Get-Content %2/current_dependents_s1.txt | Select-String -CaseSensitive -Pattern dependencies -Context 0,999 > %2/current_dependents_s2.txt"
 powershell -command "Get-Content %2/current_dependents_s2.txt | Select-String -CaseSensitive -Pattern Summary -Context 999,0 > %2/current_dependents_s3.txt" 
