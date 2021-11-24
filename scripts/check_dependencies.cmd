c6a2c4ede27afe3a4dd71b0ded7d4aec45527646
Vladimir Sumarov <summeroff@gmail.com>
2021-11-24 14:52:12 -0800

    fail on deps check

--- scripts/check_dependencies.cmd
+++ scripts/check_dependencies.cmd
@@ -3,3 +3,4 @@ dumpbin /DEPENDENTS %1 > %2/current_dependents_s1.txt
 powershell -command "Get-Content %2/current_dependents_s1.txt | Select-String -CaseSensitive -Pattern dependencies -Context 0,999 > %2/current_dependents_s2.txt"
 powershell -command "Get-Content %2/current_dependents_s2.txt | Select-String -CaseSensitive -Pattern Summary -Context 999,0 > %2/current_dependents_s3.txt" 
 powershell -command "compare-object (get-content %2/current_dependents_s3.txt) (get-content %3/cmake/approved_dependents.txt)"
+powershell -command "if( compare-object (get-content %2/current_dependents_s3.txt) (get-content  %3/cmake/approved_dependents.txt) ) { Write-Error 'Alarm: Module dependencies changed. Please review changes.' }"
