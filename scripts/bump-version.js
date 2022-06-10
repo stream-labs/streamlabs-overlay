9f808253653d8a20c38f7b1b3a587a72fb1ba70d
Vladimir Sumarov <summeroff@gmail.com>
2022-06-10 09:34:22 -0700

    fix command line

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -11,30 +11,11 @@ function log_info(msg) {
     sh.echo(colors.red(`ERROR: ${msg}`));
   }
 
-function executeCmd(cmd, options = {}) {
-    // Default is to exit on failure
-    if (options.exit == null) options.exit = true;
-  
-    const result = options.silent ? sh.exec(cmd, { silent: true }) : sh.exec(cmd);
-  
-    if (result.code !== 0) {
-      log_error(`Command Failed >>> ${cmd}`);
-      if (options.exit) {
-        sh.exit(1);
-      } else {
-        throw new Error(`Failed to execute command: ${cmd}`);
-      }
-    }
-  
-    return result.stdout;
-  }
-
-const newVersion = executeCmd('git describe --tags --abbrev=0', { silent: true })
-    .trim()
-    .replace(/^v/, '');
+const newVersion = process.argv[2]
+const packagePath = process.argv[3]
 
 try {
-    const file = path.join(process.cwd(), 'npm', 'package.json');
+    const file = path.join(process.cwd(), packagePath, 'package.json');
     const jsonData = fs.readFileSync(file);
     const root = JSON.parse(jsonData.toString());
     const currentVersion = root['version'];
